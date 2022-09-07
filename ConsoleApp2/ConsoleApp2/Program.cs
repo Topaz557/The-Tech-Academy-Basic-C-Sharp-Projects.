b775b8e951e3c3c5d5b80b5474d7a1b696bee942
Topaz557 <andreww9718@gmail.com>
2022-09-07 15:36:17 -0700

    update

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple program that asks your name and returns the name
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+
+            // this is a simple program that allows you to put in your fav number and get it added by 5 contains and example of casting
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your Favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+
+
+            // this is a simple program that uses bool and char return. uses console writeline for returning just a questionmark, actual ?
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTemperature = -23;
+            //char questionMark = '\u2103';
+
+            //Console.WriteLine(questionMark);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimetes = 211.30202092;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+            string myName = "Jesse";
+
+            int currentAge = 30;
+            string yearOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ DailyReportSummary/DailyReportSummary/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReportSummary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // asks what your name is
+            Console.WriteLine("What is your name?");
+            // sets your answer to variable yourName
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Your name is: " + yourName);
+            // writes what your name is
+            // asks the course
+            Console.WriteLine("What course are you on?");
+            string yourCourse = Console.ReadLine();
+            //turns the string yourCourse into a variable
+            Console.WriteLine("You are on course: " + yourCourse);
+            // returns the course
+            // asks what page you are on
+            Console.WriteLine("What page number?");
+            int yourPage = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("your page number is: " + yourPage);
+            // asks if you need help with anything, second line converts to a bool
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false.”?");
+            bool help = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine(help);
+            //asks question, converts your answer to a variable assinged as yourAnswer
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string yourAnswer = Console.ReadLine();
+            Console.WriteLine(yourAnswer);
+            //asks if you have any other feedback, second line converts the string answer inyo yourAnswer2
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string yourAnswer2 = Console.ReadLine();
+            Console.WriteLine(yourAnswer2);
+
+            Console.WriteLine("How many hours did you study today?");
+            int yourHours = Convert.ToInt32(Console.ReadLine());
+
+            //displays all answers in an orgnaized fashion
+            Console.WriteLine(yourName + "\n" + yourCourse + "\n" + yourPage + "\n" + help + "\n" + yourAnswer + "\n" + yourAnswer2 + "\n" + yourHours);
+            Console.WriteLine("“Thank you for your answers. An Instructor will respond to this shortly. Have a great day!”");
+            Console.ReadLine();
+        }
+    }
+}
