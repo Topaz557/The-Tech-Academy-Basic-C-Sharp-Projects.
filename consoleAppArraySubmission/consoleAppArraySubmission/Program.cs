0482c01a3d7cd8b9e67cc594afa7c2e4e0d0817b
Topaz557 <andreww9718@gmail.com>
2022-09-16 14:01:07 -0700

    update

--- consoleAppArraySubmission/consoleAppArraySubmission/Program.cs
+++ consoleAppArraySubmission/consoleAppArraySubmission/Program.cs
@@ -11,8 +11,22 @@ namespace consoleAppArraySubmission
             int[] numArray = new int[] { 5, 2, 10, 200, 5000 };
             Console.WriteLine("Pick a number between 0-4");
             int placeHolder = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(numArray[placeHolder]);
-           
+            bool holder = false;
+            while (holder == false) //KEEP AN EYE ON needed to make bools run correctly
+            {
+                if (placeHolder > 4)
+                {
+                    Console.WriteLine("Please choose a number between 0-4, we cannot process numbers higher or lower");
+                    placeHolder = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine(numArray[placeHolder]);
+                    holder = true;
+                }
+            }
+            Console.ReadLine();
+            
 
             List<string> intList = new List<string>();
             intList.Add("Hello");
@@ -24,9 +38,26 @@ namespace consoleAppArraySubmission
 
             Console.WriteLine("Pick a number between 0-5");
             int Placeholder2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(intList[Placeholder2]);
+            bool holder2 = false;
+            while (holder2 == false) // Keep eye on this code, needed to make bools run correctly
+            {
+                if (Placeholder2 > 5)
+                {
+                    Console.WriteLine("Please choose a number between 0-5, we cannot process numbers higher or lower");
+                    Placeholder2 = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine(intList[Placeholder2]);
+                    holder2 = true;
+                }
+            }
             Console.ReadLine();
 
+           
+           
+          
         }
+        
     }
 }
