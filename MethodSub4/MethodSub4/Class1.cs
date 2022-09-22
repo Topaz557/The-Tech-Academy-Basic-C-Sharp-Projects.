719de9a4a512898c7d45a1498824e0de856832c9
Topaz557 <andreww9718@gmail.com>
2022-09-22 13:33:06 -0700

    update
    sub4

---
+++ MethodSub4/MethodSub4/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSub4
+{
+    class Class1
+    {
+        public void AddNumber(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber + 5;
+            Console.WriteLine(secondNumber);
+
+        }
+    }
+}
---
+++ MethodSub4/MethodSub4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSub4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 list = new Class1();
+            list.AddNumber(6, 7);
+            list.AddNumber(firstNumber: 6, secondNumber: 8);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
