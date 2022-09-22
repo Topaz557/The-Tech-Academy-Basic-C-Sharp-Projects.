2ae7cd8499470c520a9d47a855fbe6153be4eeda
Topaz557 <andreww9718@gmail.com>
2022-09-21 20:35:00 -0700

    update

---
+++ MethodSub2/MethodSub2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSub2
+{
+    class Class1
+    {
+        public int AddNumber(int firstNumber, int secondNumber = 6)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
---
+++ MethodSub2/MethodSub2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSub2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 list = new Class1();
+            Console.WriteLine("Pick a number");
+            int NumberOne = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+
+                Console.WriteLine("Pick a second number");
+                int Numbertwo = Convert.ToInt32(Console.ReadLine());
+                int AddNumber2 = list.AddNumber(NumberOne, Numbertwo);
+                Console.WriteLine(AddNumber2);
+                Console.ReadLine();
+
+            }
+            catch
+            {
+                int AddNumber1 = list.AddNumber(NumberOne);
+                Console.WriteLine(AddNumber1);
+                Console.ReadLine();
+            }
+        }
+    }
+}
