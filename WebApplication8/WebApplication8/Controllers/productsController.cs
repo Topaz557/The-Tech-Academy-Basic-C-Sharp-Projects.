e082f521e37879fbeea0dcd67c82ef198d4a28ea
Topaz557 <andreww9718@gmail.com>
2022-10-24 14:58:41 -0700

    update

---
+++ WebApplication8/WebApplication8/Controllers/productsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class productsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
